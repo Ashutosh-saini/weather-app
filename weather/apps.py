ffec95d76ed6bd1e2e75d551eaaff8ec4c5739de
ashu <ashusaini251998@gmail.com>
2020-10-18 09:29:51 +0530

    first commit

---
+++ weather/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WeathrConfig(AppConfig):
+    name = 'weather'
---
+++ weather/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+import requests
+from django.http import Http404
+
+# Create your views here.
+
+def index(request):
+    city = request.GET.get('enteredCity')
+
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=12e3fc145f8df573ea3109d1d0d368d8'.format(city)
+
+    try:
+        city_weather = requests.get(url.format(city)).json() 
+        #request the API data and convert the JSON to Python data   types
+
+        weather = {
+            'city':city_weather['name'],    
+            'temperature': int((city_weather['main']['temp'] - 32) * 5.0/9.0),
+            'description':city_weather['weather'][0]    ['description'],
+            'icon':city_weather['weather'][0]['icon'],
+
+        }
+
+    except Exception as e:
+        raise Http404('not found')
+    
+    except KeyError:
+        raise Http404('Not found')
+
+    context = {
+        'weather':weather
+    }
+
+    return render(request, 'weather/index.html' , context) #returns the index.html template
+ 
